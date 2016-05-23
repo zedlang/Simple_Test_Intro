2b82f02eaa6643c259686e519699ce4d67185e7e
zedlang <zarehlangridge@Zarehs-MacBook-Air.local>
2016-05-23 11:59:50 +0100

    Created code to make tests work

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,19 @@
 class Calculator
   attr_accessor :accumulator
+
+  def add(val)
+    self.accumulator += val
+  end
+
+  def sub(val)
+    self.accumulator -= val
+  end
+
+  def mult(val)
+    self.accumulator *= val
+  end
+
+  def div(val)
+    self.accumulator /= val
+  end
 end
