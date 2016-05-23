aa66c05a6979146d5fe8f7571715fa77c4fe01f7
zedlang <zarehlangridge@Zarehs-MacBook-Air.local>
2016-05-23 11:57:36 +0100

    Created specs for add,subtract,multiply and divide

--- calculator_spec.rb
+++ calculator_spec.rb
@@ -7,6 +7,30 @@ class CalculatorSpec < MiniTest::Test
     my_calculator = Calculator.new
     my_calculator.accumulator = 5
     assert_equal(my_calculator.accumulator,5)
-    
   end
+
+  def test_add
+    my_calculator = Calculator.new
+    my_calculator.accumulator = 5
+    assert_equal(my_calculator.add(7),12)
+  end
+
+  def test_sub
+    my_calculator = Calculator.new
+    my_calculator.accumulator = 5
+    assert_equal(my_calculator.sub(5),0)
+  end
+
+  def test_mult
+    my_calculator = Calculator.new
+    my_calculator.accumulator = 5
+    assert_equal(my_calculator.mult(10),50)
+  end
+
+  def test_div
+    my_calculator = Calculator.new
+    my_calculator.accumulator = 5
+    assert_equal(my_calculator.div(5,1)
+  end
+
 end
